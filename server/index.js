876386763f06f44fe3aeb13d84f039c182b4ab91
Aiman Samad <aiman.samad@zemoga.com>
2021-05-30 00:27:54 -0500

    Modularize Express and added config file

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 
-const port = 3000;
-
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
-});
+module.exports = app;
