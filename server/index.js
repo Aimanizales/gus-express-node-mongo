ff018d80b0cf790800a7ca3f88d6b0bc4ed83964
Aiman Samad <aiman.samad@zemoga.com>
2021-07-13 16:09:13 -0500

    updated express GET route

--- server/index.js
+++ server/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', (req, res, next) => {
+    res.send('Welcome to the API!!!');
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message } = err;
+    res.status(statusCode);
+    res.json({
+        message,
+    });
 });
 
 module.exports = app;
